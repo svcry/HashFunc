eb5bcc3018f5ff20ca096fd5c2cdec9b0613ecc7
Akimov Aleksey <svcrifice@yandex.ru>
2024-04-03 13:45:50 +0700

    fix

--- HashFunc/MainWindow.xaml.cs
+++ HashFunc/MainWindow.xaml.cs
@@ -29,25 +29,27 @@ namespace HashFunc
 
         private void GenerateRandomNumbersButton_Click(object sender, RoutedEventArgs e)
         {
-            string input = InputTextBox.Text;
+            /*string input = InputTextBox.Text;
             if (!string.IsNullOrEmpty(input))
             {
                 byte[] hash = ComputeHash(input);
                 string hashString = BitConverter.ToString(hash).Replace("-", "");
-                HashResultTextBlock.Text = hashString;
+                HashResultTextBlock.Text = hashString;*/
 
                 RandomNumbersListBox.Items.Clear();
-                for (int i = 0; i < hashString.Length - 3; i += 4)
+                for (int i = 0; i < 21; i ++)
                 {
-                    string substring = hashString.Substring(i, 4);
-                    int randomNumber = int.Parse(substring, System.Globalization.NumberStyles.HexNumber);
-                    RandomNumbersListBox.Items.Add(new TextBlock { Text = $"Substring {i / 4 + 1}: {substring}, Random Number: {randomNumber}" });
+                    //string substring = hashString.Substring(i, 4);
+                    byte[] hash = ComputeHash(i.ToString());
+                    string hashString = BitConverter.ToString(hash).Replace("-", "");
+                    int randomNumber = int.Parse(hashString.Substring(0,4), System.Globalization.NumberStyles.HexNumber);
+                    RandomNumbersListBox.Items.Add(new TextBlock { Text = $"HashSubstr {i} : {hashString.Substring(0, 4)}, Random Number: {randomNumber}" });
                 }
-            }
+            /*}
             else
             {
                 MessageBox.Show("Please enter some text to generate random numbers.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            }*/
         }
 
 
